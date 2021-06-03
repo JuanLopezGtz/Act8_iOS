de1a765017f25b977bb7a54bfd88cd87ae289271
FlutLab.io <>
2021-06-03 20:25:09 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/act7/lopez/MainActivity.kt
@@ -0,0 +1,12 @@
+package act7.lopez
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
